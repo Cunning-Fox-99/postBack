48e6e9260f057c1c3ea96f9474397c01fb1b0e6e
Cunning-Fox-99 <kostya.moroz.96@gmail.com>
2022-10-20 23:50:22 +0300

    fixes

--- validations.js
+++ validations.js
@@ -9,12 +9,12 @@ export const registerValidation = [
     body('email', 'Email is wrong').isEmail(),
     body('password', 'Password length must be longer then 5').isLength({min: 5}),
     body('fullName', 'Name must be longer then 3').isLength({min: 3}),
-    body('avatarURL', 'Please put URL in field').optional().isURL(),
+    body('avatarUrl', 'Please put URL in field').optional(),
 ]
 
 export const postCreateValidation = [
     body('title', 'Enter title').isLength({min: 3}).isString(),
     body('text', 'Enter text').isLength({min: 5}).isString(),
     body('tags', 'Name must be longer then 3').optional().isString(),
-    body('imageUrl', 'Please put URL in field').optional().isURL(),
+    body('imageUrl', 'Please put URL in field').optional(),
 ]
